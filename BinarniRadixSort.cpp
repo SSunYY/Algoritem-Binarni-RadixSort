ab6d4d5e1ae7bd13d2f84275a692793145817da9
Martini <martin.jevsevar123@gmail.com>
2025-03-26 14:48:48 +0100

    Dodano branje iz vhodne datoteke in pisanje v izhodno.

--- BinarniRadixSort.cpp
+++ BinarniRadixSort.cpp
@@ -5,6 +5,21 @@
 using namespace std;
 
 int main(){
+    ifstream input("vhod.txt");
+    vector<unsigned char> A;
+    int temp;
+    while(input >> temp){
+        if(temp >= 0 || temp <= 255){
+            A.push_back(static_cast<unsigned char>(temp));
+        }
+    }
+    input.close();
 
+    ofstream output("izhod.txt");
+    for(unsigned char i = 0; i < A.size(); i++){
+        output << static_cast<int>(i) << " ";
+    }
+    output.close();
+    
     return 0;
 }
