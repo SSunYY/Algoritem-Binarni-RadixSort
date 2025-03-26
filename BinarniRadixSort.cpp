f3373b5e174f83edb9240580ba36f09d365e1a6f
Martini <martin.jevsevar123@gmail.com>
2025-03-26 14:55:09 +0100

    Dodano preverjanje za vhodne/izhodne datoteke.

--- BinarniRadixSort.cpp
+++ BinarniRadixSort.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main(){
     ifstream input("vhod.txt");
+    if(!input.is_open()){
+        cout << "Error: napaka pri pisanju podatkov.";
+        return 1;
+    }
     vector<unsigned char> A;
     int temp;
     while(input >> temp){
@@ -15,11 +19,17 @@ int main(){
     }
     input.close();
 
+
+
     ofstream output("izhod.txt");
+    if(!output.is_open()){
+        cerr << "Error: napaka pri branju v izhodno datoteko." << endl;
+        return 1;
+    }
     for(unsigned char i = 0; i < A.size(); i++){
         output << static_cast<int>(i) << " ";
     }
     output.close();
-    
+
     return 0;
 }
